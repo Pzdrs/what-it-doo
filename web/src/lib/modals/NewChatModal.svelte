1176a7f90f508460075edd2130eec9c74ac451ea
Petr (MBP) <petrbohac3@seznam.cz>
2025-11-09 22:16:53 +0100

    refactor: add chat modal

--- web/src/lib/modals/NewChatModal.svelte
+++ web/src/lib/modals/NewChatModal.svelte
@@ -1,16 +1,66 @@
-<script>
-	import TagsInput from '$lib/components/TagsInput.svelte';
+<script lang="ts">
+	import { t } from "svelte-i18n";
+
+	let emails = $state(['']);
+
+	function addEmail() {
+		emails = [...emails, ''];
+	}
+
+	function removeEmail(index: number) {
+		emails = emails.filter((_, i) => i !== index);
+	}
+
+	function updateEmail(index: number, value: string) {
+		emails[index] = value;
+	}
+
+	function startChat() {
+		const validEmails = emails.filter((e) => e.trim() !== '');
+		console.log(validEmails);
+		(document.getElementById('new-chat-dialog') as HTMLDialogElement)?.close();
+	}
 </script>
 
-<dialog id="new-chat-dialog" class="modal">
-	<div class="modal-box h-6/12 flex w-6/12 max-w-none flex-col">
+<dialog id="new-chat-dialog" class="modal" onclose={() => (emails = [''])}>
+	<div class="modal-box h-6/12 w-6/12 max-w-none flex flex-col">
 		<div class="flex-grow">
-			<h3 class="text-lg font-bold">Start a new chat</h3>
-			<p class="py-4">Please select the people you want to chat with:</p>
-			<TagsInput />
+			<h3 class="text-lg font-bold">{$t('start_chat_long')}</h3>
+			<p class="py-4">{$t('enter_emails_prompt')}</p>
+
+			<div class="flex flex-col gap-2">
+				{#each emails as email, i}
+					<div class="flex gap-2 items-center">
+						<input
+							type="email"
+							placeholder="{$t('email_placeholder')}"
+							class="input input-bordered w-full"
+							bind:value={emails[i]}
+							oninput={(e) => updateEmail(i, e.target.value)}
+							required
+						/>
+						{#if emails.length > 1}
+							<button
+								type="button"
+								class="btn btn-error btn-sm"
+								onclick={() => removeEmail(i)}
+							>
+								{$t('remove')}
+							</button>
+						{/if}
+					</div>
+				{/each}
+
+				<button type="button" class="btn btn-outline mt-2" onclick={addEmail}>
+					{$t('add_another_email')}
+				</button>
+			</div>
 		</div>
-		<div class="modal-action mt-max">
-			<a href="#" class="btn">Start Chat</a>
+
+		<div class="modal-action">
+			<button class="btn btn-primary" type="button" onclick={startChat}>
+				{$t('start_chat')}
+			</button>
 		</div>
 	</div>
 
